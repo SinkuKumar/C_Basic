023f58c3c02efcf7e24af16e3de01b70d99aaaca
Sinku Kumar <sudosinku@gmail.com>
2021-10-18 19:51:06 +0530

    Add project files.

---
+++ Personal_Info/Personal_Info.c
@@ -0,0 +1,16 @@
+/* 
+*	Personal_Info.c : C program to print your name, date of birth, and mobile number.
+
+*	Expected Output:
+	Name  : Sinku Kumar
+	DOB	  : Nov 22, 1997
+	Mobile: 91-9876543210
+*/
+
+#include<stdio.h>
+
+int main(void) {
+	printf("Name   : Sinku Kumar\n");
+	printf("DOB    : Nov 22, 1997\n");
+	printf("Mobile : 91-9876543210\n");
+}
